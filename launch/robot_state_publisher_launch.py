bcc6f23e2e11f7599efe2d842316972d148414cc
Pierre-Yves Malengré <malengre.py@gmail.com>
2025-01-17 23:35:09 +0100

    Initial commit, displaying in rviz

---
+++ launch/robot_state_publisher_launch.py
@@ -0,0 +1,28 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        # Joint State Publisher
+        Node(
+            package='joint_state_publisher',
+            executable='joint_state_publisher',
+            name='joint_state_publisher',
+            output='screen',
+        ),
+        # Robot State Publisher
+        Node(
+            package='robot_state_publisher',
+            executable='robot_state_publisher',
+            name='robot_state_publisher',
+            output='screen',
+            parameters=[{'robot_description': open('/home/py/ros2_ws/src/gazebo_test/urdf/robot.urdf').read()}]
+        ),
+        # RViz2
+        Node(
+            package='rviz2',
+            executable='rviz2',
+            arguments=['-d', '/home/py/ros2_ws/src/gazebo_test/rviz/urdf.rviz'],
+            output='screen'
+        )
+    ])
